dc8eea953b83fe5c3e47ab45d4f1e0d5af1ceffd
Prabhat Kumar <60351919+prabhatk7@users.noreply.github.com>
2020-07-08 18:29:44 +0530

    Update create-api-client.js

--- src/api/create-api-client.js
+++ src/api/create-api-client.js
@@ -1,7 +1,7 @@
-import Firebase from 'firebase/app'
+import Firebase from 'firebase/app'                     
 import 'firebase/database'
 
-export function createAPI ({ config, version }) {
-  Firebase.initializeApp(config)
-  return Firebase.database().ref(version)
+export function createAPI ({ config, version }) {             //export createAPI function which takes an object with config and version parameters
+  Firebase.initializeApp(config)                              //firebase is initialized with provided config
+  return Firebase.database().ref(version)                     //return firebase database
 }
