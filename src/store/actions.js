064478d0c65ffc26b3e02cc24a2cb36c7c8173c5
Prabhat Kumar <60351919+prabhatk7@users.noreply.github.com>
2020-07-08 19:32:20 +0530

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,26 +1,26 @@
-import {
+import {                            //import from api
   fetchUser,
   fetchItems,
   fetchIdsByType
 } from '../api'
 
-export default {
+export default {                                                         //object with functions is exported
   // ensure data for rendering given list type
-  FETCH_LIST_DATA: ({ commit, dispatch, state }, { type }) => {
-    commit('SET_ACTIVE_TYPE', { type })
-    return fetchIdsByType(type)
-      .then(ids => commit('SET_LIST', { type, ids }))
-      .then(() => dispatch('ENSURE_ACTIVE_ITEMS'))
+  FETCH_LIST_DATA: ({ commit, dispatch, state }, { type }) => {          //for fetching list data
+    commit('SET_ACTIVE_TYPE', { type })                                  //set active type as type
+    return fetchIdsByType(type)                                         //fetchIds by type and return
+      .then(ids => commit('SET_LIST', { type, ids }))                   //then set list 
+      .then(() => dispatch('ENSURE_ACTIVE_ITEMS'))                       //then dispatch
   },
 
   // ensure all active items are fetched
-  ENSURE_ACTIVE_ITEMS: ({ dispatch, getters }) => {
+  ENSURE_ACTIVE_ITEMS: ({ dispatch, getters }) => {                       //fetchfor all active Ids
     return dispatch('FETCH_ITEMS', {
       ids: getters.activeIds
     })
   },
 
-  FETCH_ITEMS: ({ commit, state }, { ids }) => {
+  FETCH_ITEMS: ({ commit, state }, { ids }) => {                                 //fetch items by Ids
     // on the client, the store itself serves as a cache.
     // only fetch items that we do not already have, or has expired (3 minutes)
     const now = Date.now()
@@ -35,13 +35,13 @@ export default {
       return false
     })
     if (ids.length) {
-      return fetchItems(ids).then(items => commit('SET_ITEMS', { items }))
+      return fetchItems(ids).then(items => commit('SET_ITEMS', { items }))         //return fetch items and then set items
     } else {
-      return Promise.resolve()
+      return Promise.resolve()                                        
     }
   },
 
-  FETCH_USER: ({ commit, state }, { id }) => {
+  FETCH_USER: ({ commit, state }, { id }) => {                                 //fetchUser by ids
     return state.users[id]
       ? Promise.resolve(state.users[id])
       : fetchUser(id).then(user => commit('SET_USER', { id, user }))
